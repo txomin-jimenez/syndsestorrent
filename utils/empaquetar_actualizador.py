4f520a161e8183779a4a8194d4bbf01e3c692ec4
luskaner <luskaner@gmail.com>
2014-07-19 22:51:35 +0200

    Ahora se empaqueta correctamente los permisos de los ficheros del actualizador

--- utils/empaquetar_actualizador.py
+++ utils/empaquetar_actualizador.py
@@ -1,4 +1,5 @@
 #! /usr/bin/python
+# -*- coding: utf-8 -*-
 
 # Copyright (C) 2014 David
 #
@@ -16,7 +17,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os.path
-from empaquetar_modulo import construir_tar_gz
+import tarfile
 from empaquetar_lanzamiento import borrar_contenido_carpeta
 
 __author__="David"
@@ -31,6 +32,23 @@ ruta_inf = os.path.join(ruta_base, 'INFO')
 ruta_lic = os.path.join(ruta_base, 'LICENSE')
 ruta_scr = os.path.join(ruta_base, 'scripts')
 
+def set_permissions(tarinfo):
+    tarinfo.mode = 511
+    return tarinfo
+
+def construir_tar_gz(*arg):
+    """
+    Hace un fichero comprimido .tar.gz como nombre arg(0) y añadiendo dentro
+    el resto de parámetros como ficheros
+    """
+    if os.path.isfile(arg[0]):
+        os.remove(arg[0])
+        
+    with tarfile.open(arg[0], "w:gz") as tar:
+        for f in xrange(1, len(arg)):
+            split_path = arg[f].split(os.sep)
+            tar.add(arg[f], split_path[len(split_path) - 1], filter=set_permissions)
+            
 def construir_paq_base():
     construir_tar_gz(ruta_pq_b, os.path.join(ruta_base, 'codigo'))
     
