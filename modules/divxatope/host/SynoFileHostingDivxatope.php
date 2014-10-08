b9b5f24419ff1144cbaec0093967a8247fc557df
David Fernández Aldana <luskaner@gmail.com>
2014-10-08 22:34:27 +0200

    Actualizado el módulo host de DivxaTope para ajustarse a cambios en la página

--- modules/divxatope/host/SynoFileHostingDivxatope.php
+++ modules/divxatope/host/SynoFileHostingDivxatope.php
@@ -1,20 +1,20 @@
 <?php
 
 /*
-    This file is part of SynDsEsTorrent.
+  This file is part of SynDsEsTorrent.
 
-    SynDsEsTorrent is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+  SynDsEsTorrent is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    SynDsEsTorrent is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  SynDsEsTorrent is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with SynDsEsTorrent.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with SynDsEsTorrent.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace modules\divxatope\host;
@@ -63,12 +63,29 @@ class SynoFileHostingDivxatope
         curl_setopt($curl, CURLOPT_URL, $this->url);
 
         $dlPage = curl_exec($curl);
-        $regexpUrl = "(http:\/\/tumejorserie.*)\"";
 
-        $matchesUrl = array();
         $ret = '';
-        if (preg_match("/$regexpUrl/iU", $dlPage, $matchesUrl)) {
-            $ret = $matchesUrl[1];
+        $resultadoPreUrl = [];
+        if (preg_match(
+            '/<a.*href="(http:\/\/(www.)?divxatope.com\/descarga-torrent\/.*)".*>Descarga Torrent<\/a>/siU',
+            $dlPage,
+            $resultadoPreUrl
+        )) {
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($curl, CURLOPT_USERAGENT, DOWNLOAD_STATION_USER_AGENT);
+            curl_setopt($curl, CURLOPT_HEADER, true);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_URL, $resultadoPreUrl[1]);
+
+            $dlPageTorrent = curl_exec($curl);
+            $regexpUrl = "(http:\/\/tumejorserie.*)\"";
+            $matchesUrl = array();
+
+            if (preg_match("/$regexpUrl/iU", $dlPageTorrent, $matchesUrl)) {
+                $ret = $matchesUrl[1];
+            }
         }
 
         return $ret;
